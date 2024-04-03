0891b56d9c93d0249e9f8c54ad5ec8950c98c1b1
DennisD2 <info@spurtikus.de>
2024-04-03 13:59:26 +0200

    Working test code, tested in WineApe. Dumps out some code lines.

--- disassembler-test/dtest.asm
+++ disassembler-test/dtest.asm
@@ -1,50 +1,51 @@
-
+; ---------------------------------
+; disassemble some piece of memory
+; ---------------------------------
 	    org &8000
 
-        PrintChar 	equ	#bb5a
-mymain:
-        ld      hl,&8000
-        ld      bc,&9
-        ld      a,0
-        call    StrInitialise
+        PrintChar 	equ	    #bb5a
 
+mymain:
+        ld      hl,&8000    ; start address for disassembing
+        ld      bc,&10      ; number of op codes to disassemble, "lines"
+
+; disassemble code
+; hi is start of code, bc number of opcodes to disassemble
 disloop:
-        ; call with address to disassemble
+        ; DisWrInstruction, call with address to disassemble in hl
         call    DisWrInstruction
         ; a = length of instruction
         ; hl = address of next instruction
         ; kStrBuffer contains disassembled opcode string
 
         call    print_string
-        dec     bc
-        ld      a,b
+        dec     bc              ; lines--
+        ld      a,b             ; line loop finished?
         or      c
-        jr      nz,disloop
-        ret
+        jr      nz,disloop      ; no, continue
+        ret                     ; yes, we're done
 
+; print a string located at kStrBuffer
 print_string:
-        push    hl
+        push    hl              ; save hl
 
         ld      hl,kStrBuffer
-lp:     ld      a,(hl)
+        ld      b,(hl)          ; length of string
+        inc     b
         inc     hl
-        or      a
-        jr      z,done
-	    push 	hl		; save hl
-	    push 	de		; move de to hl
-	    pop     hl
+lp:     ld      a,(hl)          ; next char to print
+        inc     hl
+        dec     b               ; i--
+        jr      z,done          ; i==0?, yes jump to done
 	    call    PrintChar
-	    inc 	de
-	    pop	    hl		; restore hl
         jp      lp
 done:
         call    NewLine
-        ld      a,0
-        call    StrInitialise
 
-        pop hl
+        pop hl                  ; restore hl
         ret
 
+; print a newline char
 NewLine:
         ld      a,13
         call    PrintChar
@@ -53,6 +54,6 @@ NewLine:
         ret
 
 message:
-        defb 'Hello, world.',0
 
+        ; include disassembler source
 	    read "disassembler.asm"
