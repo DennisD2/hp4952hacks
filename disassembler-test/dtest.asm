0fab1c398cb04d1e42983cdda396bbb001b4472f
DennisD2 <info@spurtikus.de>
2024-04-03 12:26:47 +0200

    Added test code, work in progress

--- disassembler-test/dtest.asm
+++ disassembler-test/dtest.asm
@@ -2,14 +2,29 @@
 	    org &8000
 
         PrintChar 	equ	#bb5a
-
 mymain:
-        ld hl,&8000
-        call DisWrInstruction
-        call print_string
+        ld      hl,&8000
+        ld      bc,&9
+        ld      a,0
+        call    StrInitialise
+
+disloop:
+        ; call with address to disassemble
+        call    DisWrInstruction
+        ; a = length of instruction
+        ; hl = address of next instruction
+        ; kStrBuffer contains disassembled opcode string
+
+        call    print_string
+        dec     bc
+        ld      a,b
+        or      c
+        jr      nz,disloop
         ret
 
 print_string:
+        push    hl
+
         ld      hl,kStrBuffer
 lp:     ld      a,(hl)
         inc     hl
@@ -22,7 +37,21 @@ lp:     ld      a,(hl)
 	    inc 	de
 	    pop	    hl		; restore hl
         jp      lp
-done:   ret
+done:
+        call    NewLine
+        ld      a,0
+        call    StrInitialise
+
+        pop hl
+        ret
+
+NewLine:
+        ld      a,13
+        call    PrintChar
+        ld      a,10
+        call    PrintChar
+        ret
+
 message:
         defb 'Hello, world.',0
 
